d601671731706f974bb389300445f6d95e0dca74
Alexey Karazhey <grimreio@gmail.com>
2013-02-15 17:03:09 +0200

    PRAY to run

--- dev/Game/proj.android/src/org/cocos2dx/hellocpp/HelloCpp.java
+++ dev/Game/proj.android/src/org/cocos2dx/hellocpp/HelloCpp.java
@@ -27,7 +27,7 @@ import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.os.Bundle;
 
-public class CrossTest extends Cocos2dxActivity{
+public class HelloCpp extends Cocos2dxActivity{
 
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
